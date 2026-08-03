8f890834dbef9a768f93cfbcbc7eed9f30526ca8
Erald Haklaj <49836914+eraldhaklaj@users.noreply.github.com>
2026-08-03 19:37:14 +0200

    Add web/src/main.tsx

---
+++ web/src/main.tsx
@@ -0,0 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles.css";
+
+const client = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false, staleTime: 2000 } },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>,
+);
